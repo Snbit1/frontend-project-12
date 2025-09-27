db4ae247eaf0f2146ac7513b82f35f8845e3b6ad
Александр <k.sasha.228228@gmail.com>
2025-09-27 20:01:31 +0300

    Add linter

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,64 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import functional from 'eslint-plugin-functional'
+import testingLibrary from 'eslint-plugin-testing-library'
+import prettier from 'eslint-plugin-prettier'
+import { defineConfig } from 'eslint/config'
+import babelParser from '@babel/eslint-parser'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  {
+    ignores: ['node_modules', 'dist', 'build'],
+  },
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: babelParser,
       parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react'],
+        },
         ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      functional,
+      'testing-library': testingLibrary,
+      prettier,
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'prettier/prettier': 'error',
+      'import/extensions': 0,
+      'import/no-unresolved': 0,
+      'react/prop-types': 0,
+      'no-console': 0,
+      'react/react-in-jsx-scope': 0,
+      'functional/no-conditional-statements': 0,
+      'functional/no-expression-statements': 0,
+      'functional/immutable-data': 0,
+      'functional/functional-parameters': 0,
+      'functional/no-try-statements': 0,
+      'functional/no-throw-statements': 0,
+      'functional/no-return-void': 0,
+      'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
+      'react/function-component-definition': [
+        'error',
+        { namedComponents: 'arrow-function' },
+      ],
+      'testing-library/no-debug': 0,
+      'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
     },
   },
 ])
+
+
+
+
