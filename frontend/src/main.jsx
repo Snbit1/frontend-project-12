efd17782e40d1f214f5c55040616423bc0a8111f
Александр <k.sasha.228228@gmail.com>
2025-10-20 18:29:20 +0300

    Step 11

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,26 +10,39 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
 import { initProfanity } from './utils/profanity'
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react'
 
 initProfanity()
+
+const rollbarConfig = {
+  accessToken:
+    '7885b65ccf2141829a88107fccd9221e5e3131514146b3a1227b60962fbed63336adb0f6ec631ab85c816dd810c8b755',
+  environment: 'development',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+}
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop
-          closeOnClick
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </BrowserRouter>
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop
+              closeOnClick
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+            />
+          </BrowserRouter>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   </React.StrictMode>
 )
